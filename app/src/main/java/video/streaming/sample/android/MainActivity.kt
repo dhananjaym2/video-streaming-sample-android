dadeca7807f911f1389d47bc150902d5498b6288
Dhananjay <dhananjaymohnot@gmail.com>
2018-12-14 08:55:33 +0530

    incomplete UI and functionality

--- app/src/main/java/video/streaming/sample/android/MainActivity.kt
+++ app/src/main/java/video/streaming/sample/android/MainActivity.kt
@@ -2,8 +2,16 @@ package video.streaming.sample.android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.textView_StreamVideo {
+
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
